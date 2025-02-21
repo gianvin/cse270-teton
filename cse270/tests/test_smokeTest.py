53af09dae4e8a6c1818b925c77efe633934a917f
gianvin <ginamangahassanchezong@gmail.com>
2025-02-21 16:49:03 +0800

    edited the smoke pytest

--- cse270/tests/test_smokeTest.py
+++ cse270/tests/test_smokeTest.py
@@ -9,13 +9,13 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless=new")
-    self.driver = webdriver.Firefox(options=options)
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
     self.base_url = "http://127.001/cse270/teton/1.6/index.html"
     
